433d473f2384ec4d054c187ce38bf4353dca55fc
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2024-08-30 03:35:15 +0300

    1

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css"
+// import typescriptLogo from "./typescript.svg"
+// import viteLogo from "/vite.svg"
+// import { setupCounter } from "./counter.ts"
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+<h1>Для изучения TypeScript неделя? НЕДЕЛЯ, Карл!</h1>
+  </div>
+`
